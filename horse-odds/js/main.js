2eeef3120c0b6afc73c566af38c7c7e40c4f57ec
K Adam White <kadamwhite@users.noreply.github.com>
2020-01-02 23:00:20 -0500

    INtroduce eslint and editorconfig for my own sanity

--- horse-odds/js/main.js
+++ horse-odds/js/main.js
@@ -1,100 +1,99 @@
-var form = document.querySelector('form.horse-odds');
+let form = document.querySelector( 'form.horse-odds' );
 
-function oddsToPercentage(odds) {
-  if (! odds ) {
+function oddsToPercentage ( odds ) {
+  if ( ! odds ) {
     return 0;
   }
-  return 1 / (odds + 1);
+  return 1 / ( odds + 1 );
 }
 
-function getHorseOdds() {
-  var horseInputNodes = [].slice.call(document.querySelectorAll('.horse-odds__input'));
-  return horseInputNodes.map(function(node) {
-    return node.value ? +node.value : 0;
-  });
+function getHorseOdds () {
+  // eslint-disable-next-line prefer-reflect
+  let horseInputNodes = [].slice.call( document.querySelectorAll( '.horse-odds__input' ) );
+  return horseInputNodes.map( ( node ) => {
+    return node.value ? Number( node.value ) : 0;
+  } );
 }
 
-function getHorsePercentages() {
-  return getHorseOdds().map(oddsToPercentage);
+function getHorsePercentages () {
+  return getHorseOdds().map( oddsToPercentage );
 }
 
-function getTotalPercentages() {
-  return getHorsePercentages().reduce(function(sum, pct) {
+function getTotalPercentages () {
+  return getHorsePercentages().reduce( ( sum, pct ) => {
     return sum + pct;
-  }, 0);
+  }, 0 );
 }
 
-function getHorseOutputNode(idx) {
-  return document.querySelector(`.horse-odds__percentage[data-horse="${ idx + 1 }"]`);
+function getHorseOutputNode ( idx ) {
+  return document.querySelector( `.horse-odds__percentage[data-horse="${ idx + 1 }"]` );
 }
 
-document.querySelectorAll('.horse-odds__input').forEach(function(input) {
-  input.addEventListener('focus', function(evt) {
+document.querySelectorAll( '.horse-odds__input' ).forEach( ( input ) => {
+  input.addEventListener( 'focus', ( evt ) => {
     evt.target.value = '';
-  });
-});
+  } );
+} );
 
-function toPct(val, digits) {
-  return `${ (val * 100).toFixed(digits || 2)}%`;
+function toPct ( val, digits ) {
+  return `${ ( val * 100 ).toFixed( digits || 2 ) }%`;
 }
 
-form.addEventListener('change', function(evt) {
-  var value = evt.target.value;
-  var horse = evt.target.dataset.horse;
+form.addEventListener( 'change', () => {
 
   // Update posted percentages
-  var percentages = getHorsePercentages();
-  console.log(percentages);
+  let percentages = getHorsePercentages();
+  console.log( percentages );
 
   // Update total.
-  var total = getTotalPercentages();
-  var oddsSummary = document.querySelector('.horse-odds__total-percentage');
-  if (total < 0.8) {
+  let total = getTotalPercentages();
+  let oddsSummary = document.querySelector( '.horse-odds__total-percentage' );
+  if ( total < 0.8 ) {
     oddsSummary.innerHTML = 'Odds are very good.';
-  } else if (total < 1) {
+  } else if ( total < 1 ) {
     oddsSummary.innerHTML = 'Odds are better than posted.';
   } else {
     oddsSummary.innerHTML = 'Odds are worse than posted.';
   }
 
   // Update per-horse odds.
-  var actuals = [];
-  percentages.forEach(function(pct, idx) {
-    var output = getHorseOutputNode(idx);
+  let actuals = [];
+  percentages.forEach( ( pct, idx ) => {
+    let output = getHorseOutputNode( idx );
 
-    if (! pct ) {
+    if ( ! pct ) {
       output.innerHTML = '';
       return;
     }
 
-    var pctAdjustment = (pct / total) * (1 - total );
-    var actualPct = pct + pctAdjustment;
-    output.innerHTML = `(${ toPct(actualPct, 1) })`;
-    if (actualPct > 0.4) {
+    let pctAdjustment = ( pct / total ) * ( 1 - total );
+    let actualPct = pct + pctAdjustment;
+    output.innerHTML = `(${ toPct( actualPct, 1 ) })`;
+    if ( actualPct > 0.4 ) {
       output.innerHTML = `<strong>${ output.innerHTML }</strong>`;
     }
 
-    actuals.push(actualPct);
-  });
+    actuals.push( actualPct );
+  } );
 
   // Figure out best horse.
-  var best = {
-    horse: [0],
+  let best = {
+    horse: [ 0 ],
     pct: -Infinity,
   };
-  actuals.forEach(function(pct, idx) {
-    if (best.pct < pct) {
+  actuals.forEach( ( pct, idx ) => {
+    if ( best.pct < pct ) {
       best = {
-        horse: [idx],
-        pct: pct,
+        horse: [ idx ],
+        pct,
       };
-    } else if (best.pct === pct) {
-      best.horse.push(idx);
+    } else if ( best.pct === pct ) {
+      best.horse.push( idx );
     }
-  });
-  document.querySelector('.horse-odds__advice').innerHTML = `Bet on horse${
+  } );
+  document.querySelector( '.horse-odds__advice' ).innerHTML = `Bet on horse${
     best.horse.length > 1 ? 's' : ''
-  } ${ best.horse.map(function(idx) {
+  } ${ best.horse.map( ( idx ) => {
     return `#${ idx + 1 }`;
-  }).join(' or ') }`;
-});
+  } ).join( ' or ' ) }`;
+} );
